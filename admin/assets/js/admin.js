e33a5658ec98431506122069e5e1d42eb8e84f8e
Marc Hibbins <marchibbins@gmail.com>
2014-08-13 19:28:19 +0100

    Initial javascript

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -3,7 +3,16 @@
 
 	$(function () {
 
-		console.log('Vimeo importer');
+		var config = {
+			'container': '.js-vimeo-importer',
+			'button': {
+				'classes': 'button button-primary button-large',
+				'text': 'Import Vimeo videos to Showreel'
+			}
+		};
+
+		var buttonHtml = '<button class="' + config.button.classes + '">' + config.button.text + '</button>';
+		$(config.container).append(buttonHtml);
 
 	});
 
