95012cea914d77624b88502393e6bba1dcd52b83
Marc Hibbins <marchibbins@gmail.com>
2014-08-14 16:50:27 +0100

    Add initial simple API for Vimeo importer

---
+++ api/index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin Name.
+ *
+ * @package   Vimeo_Importer_Api
+ * @author    Marc Hibbins <marc@marchibbins.com>
+ * @license   GPL-2.0+
+ * @link      http://marchibbins.com
+ */
+
+/**
+ * Api class. This class uses the Vimeo PHP library
+ * to handle calls to the Vimeo API with OAuth 2.0.
+ *
+ * @package Vimeo_Importer_Api
+ * @author  Marc Hibbins <marc@marchibbins.com>
+ */
+
+// Match only a few endpoints
+$endpoints = array(
+    'albums' => '/me/albums'
+);
+
+if ( !isset( $_GET['endpoint'] ) || !isset( $endpoints[$_GET['endpoint']] ) ) {
+    die;
+}
+
+$endpoint = $endpoints[$_GET['endpoint']];
+
+// Init Vimeo
+include_once( 'includes/vimeo.php' );
+
+$APP_ID = '';
+$APP_SECRET = '';
+$ACCESS_TOKEN = '';
+
+$vimeo = new Vimeo( $APP_ID, $APP_SECRET, $ACCESS_TOKEN );
+
+// Dump data as JSON
+$data = $vimeo->request( $endpoint );
+
+header( 'Content-Type: application/json' );
+echo json_encode( $data );
